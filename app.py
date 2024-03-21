a507e7da8aae219cc70296f0be54a7f8c0d3c513
avtandilGvenetadze <gvenetadze.avtandil@gtu.ge>
2024-03-21 20:01:54 +0400

    comment

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ def login_required(f):
     """
     Decorate routes to require login.
 
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
-    """
+    # http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    # """
+    
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
